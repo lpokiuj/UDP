bda7febdc5bf63fd1751d9dbcdd591ae9544cb9e
lpokiuj <michael082@binus.ac.id>
2021-08-20 00:16:29 +0700

    sidebar dropdown half finished, not implemented to other page.

--- JavaScript/sideBar.js
+++ JavaScript/sideBar.js
@@ -2,11 +2,27 @@
 let burger = document.querySelector('.burger-icon');
 let sidebar = document.querySelector('.sidebar');
 let closeBurger = document.querySelector('.close-burger');
+let overlay = document.querySelector('.overlay-sidebar');
 
 burger.addEventListener("click", function(){
-    sidebar.style.display = "block";
+    sidebar.style.right = "0";
+    overlay.style.display = "block";
+    overlay.style.position = "absolute";    
+    overlay.style.width= "100%";
+    overlay.style.height = "100%";
+
 });
 
 closeBurger.addEventListener("click", function(){
-    sidebar.style.display = "none";
+    sidebar.style.right = "-100%";
+    overlay.style.display = "none";
+});
+
+document.querySelector('body').addEventListener("click", function(e){
+
+    if(e.target == overlay){
+        sidebar.style.right = "-100%";
+        overlay.style.display = "none";
+    }
+
 });
