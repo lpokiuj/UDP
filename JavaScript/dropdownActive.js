a4fbdb18615393b06cd1300a31c82ea7ddcea2e4
lpokiuj <michael082@binus.ac.id>
2021-08-20 21:41:48 +0700

    api

--- JavaScript/dropdownActive.js
+++ JavaScript/dropdownActive.js
@@ -1,6 +1,5 @@
 let dropdownButtonCategories = document.querySelector('.categories-bar');
 let dropdownButtonGacha = document.querySelector('.gacha-set-bar');
-let dropdownButtonSort = document.querySelector('.sort-by-bar');
 
 let flag = 0;
 dropdownButtonCategories.addEventListener("click", function(){
@@ -11,7 +10,6 @@ dropdownButtonCategories.addEventListener("click", function(){
     else{
         dropdownButtonCategories.classList.add('active');
         dropdownButtonGacha.classList.remove('active');
-        dropdownButtonSort.classList.remove('active');
         flag = 1;
     }
 
@@ -25,21 +23,6 @@ dropdownButtonGacha.addEventListener("click", function(){
     else{
         dropdownButtonGacha.classList.add('active');
         dropdownButtonCategories.classList.remove('active');
-        dropdownButtonSort.classList.remove('active');
-        flag = 1;
-    }
-
-});
-
-dropdownButtonSort.addEventListener("click", function(){
-
-    if(dropdownButtonSort.classList.contains('active')){
-        dropdownButtonSort.classList.remove('active');
-    }
-    else{
-        dropdownButtonSort.classList.add('active');
-        dropdownButtonCategories.classList.remove('active');
-        dropdownButtonGacha.classList.remove('active');
         flag = 1;
     }
 
@@ -53,10 +36,6 @@ document.querySelector('body').addEventListener("click", function(e){
     if(dropdownButtonGacha != e.target && dropdownButtonGacha.classList.contains('active') && flag == 0){
         dropdownButtonGacha.classList.remove('active');
     }
-    if(dropdownButtonSort != e.target && dropdownButtonSort.classList.contains('active') && flag == 0){
-        dropdownButtonSort.classList.remove('active');
-    }
-    
     flag = 0;
 
 });
