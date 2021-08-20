f089695a4dd07564f10d609fdeacf07479703a99
lpokiuj <michael082@binus.ac.id>
2021-08-20 20:46:18 +0700

    sidebar dropdown made and is implemented in all pages

--- JavaScript/sideBar.js
+++ JavaScript/sideBar.js
@@ -16,6 +16,8 @@ burger.addEventListener("click", function(){
 closeBurger.addEventListener("click", function(){
     sidebar.style.right = "-100%";
     overlay.style.display = "none";
+    categories.classList.remove('active');
+    gacha.classList.remove('active');
 });
 
 document.querySelector('body').addEventListener("click", function(e){
@@ -23,6 +25,49 @@ document.querySelector('body').addEventListener("click", function(e){
     if(e.target == overlay){
         sidebar.style.right = "-100%";
         overlay.style.display = "none";
+        categories.classList.remove('active');
+        gacha.classList.remove('active');
     }
+
+});
+
+let categories = document.querySelector('.drop-down-button-sidebar-categories');
+let gacha = document.querySelector('.drop-down-button-sidebar-gacha');
+
+categories.addEventListener("click", function(){
+
+    if(categories.classList.contains('active')){
+        categories.classList.remove('active');
+    }
+    else{
+        categories.classList.add('active');
+        gacha.classList.remove('active');
+        flag = 1;
+    }
+
+});
+
+gacha.addEventListener("click", function(){
+
+    if(gacha.classList.contains('active')){
+        gacha.classList.remove('active');
+    }
+    else{
+        gacha.classList.add('active');
+        categories.classList.remove('active');
+        flag = 1;
+    }
+
+});
+
+document.querySelector('.sidebar').addEventListener("click", function(e){
+
+    if(categories != e.target && categories.classList.contains('active') && flag == 0){
+        categories.classList.remove('active');
+    }
+    if(gacha != e.target && gacha.classList.contains('active') && flag == 0){
+        gacha.classList.remove('active');
+    }
+    flag = 0;
 
 });
