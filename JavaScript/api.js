700a7f48b6043728ebdb907af424b80b08e4bd0f
SusuMantan <suzumantan@gmail.com>
2021-08-21 21:36:31 +0700

    fix: add uberId to fetch data

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -2,7 +2,7 @@ const fetchData = () => {
     return fetch("../db/db.mock.json")
       .then((response) => response.json())
       .then((responseJson) =>
-        Object.entries(responseJson).map(([_, value]) => value)
+        Object.entries(responseJson).map(([key, value]) => ({...value, uberId: parseInt(key, 10)}))
       )
       .catch((err) => {
         alert("there's an error when trying to fetch data. See logging");
